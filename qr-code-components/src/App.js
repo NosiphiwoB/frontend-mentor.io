e33a5932d010d62fc5f8b214b6567903c860e54f
NosiphiwoB <biyelanosiphiwo@gmail.com>
2023-02-17 15:30:14 +0200

    qr code

---
+++ qr-code-components/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="container">
+        <div>
+        <img id="image" src="./assets/image-qr-code.png" />
+        </div>
+
+        <div>
+         <h2>Improve your front-end skills by building projects</h2>
+        </div>
+
+        <div>
+            <p>Scan the QR code to visit Frontend and take your coding skills to the next level</p>
+        </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
