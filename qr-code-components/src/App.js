ead5ce4788283e94a583a6b3de8e8f43ce3d80f9
NosiphiwoB <biyelanosiphiwo@gmail.com>
2023-02-21 15:34:03 +0200

    css

--- qr-code-components/src/App.js
+++ qr-code-components/src/App.js
@@ -11,12 +11,14 @@ function App() {
         </div>
 
       <div >
-        <div>
+        <div id="header">
          <h2>Improve your front-end skills by building projects</h2>
         </div>
 
         <div>
-            <p>Scan the QR code to visit Frontend and take your coding skills to the next level</p>
+            <p>Scan the QR code to visit Frontend 
+            <br/>Mentor and take your coding skills to
+            <br/>the next level</p>
         </div>
     </div>
    </div>
